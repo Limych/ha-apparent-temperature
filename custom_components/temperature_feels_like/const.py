caa5bcab3c5316bee881ee88b1a85b5bda8c1b1c
Andrey Khrolenok <andrey@khrolenok.ru>
2021-04-13 14:44:01 +0300

    Bump version to 0.1.0-beta

--- custom_components/temperature_feels_like/const.py
+++ custom_components/temperature_feels_like/const.py
@@ -3,7 +3,7 @@
 # Base component constants
 NAME = "Temperature Feels Like"
 DOMAIN = "temperature_feels_like"
-VERSION = "0.1.0.dev0"
+VERSION = "0.1.0-beta"
 ISSUE_URL = "https://github.com/Limych/ha-temperature-feeling/issues"
 
 STARTUP_MESSAGE = f"""
