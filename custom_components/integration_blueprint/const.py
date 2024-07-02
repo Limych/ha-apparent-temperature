9dfb15e4462f12118e79466ce5b37ec3033f5a86
Andrey Khrolenok <andrey@khrolenok.ru>
2024-07-02 22:59:56 +0300

    Restore in const.py constants NAME and VERSION

--- custom_components/integration_blueprint/const.py
+++ custom_components/integration_blueprint/const.py
@@ -1,9 +1,12 @@
 """Constants for integration_blueprint."""
 
 from logging import Logger, getLogger
+from typing import Final
 
 LOGGER: Logger = getLogger(__package__)
 
-DOMAIN = "integration_blueprint"
-ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
-ISSUE_URL = "https://github.com/Limych/ha-blueprint/issues"
+NAME: Final = "Integration blueprint"
+DOMAIN: Final = "integration_blueprint"
+VERSION: Final = "0.0.0"
+ATTRIBUTION: Final = "Data provided by http://jsonplaceholder.typicode.com/"
+ISSUE_URL: Final = "https://github.com/Limych/ha-blueprint/issues"
